639cea240bad089819ea84bcf97d127b1e42e646
shiv <shivbhosale97@gmail.com>
2018-01-11 23:19:08 +0530

    recursive tree traversals added

---
+++ DataStructures/tree_traversal.cpp
@@ -0,0 +1,88 @@
+/*
+this code is to implement all my tree traversals
+*/
+#include <iostream>
+
+typedef struct node{
+	char data;
+	node *left;
+	node *right;
+}node;
+
+node *make_tree(char to_push[9]){
+	node *a = (node*)malloc(sizeof(node));
+	a->data = to_push[0];
+	node *b = (node*)malloc(sizeof(node));
+	b->data = to_push[1];
+	a->left = b;
+	node *c = (node*)malloc(sizeof(node));
+	c->data = to_push[2];
+	a->right = c;
+	b->left = NULL;
+	node *d = (node*)malloc(sizeof(node));
+	b->right = d;
+	d->data = to_push[3];
+	node *e = (node*)malloc(sizeof(node));
+	node *f = (node*)malloc(sizeof(node));
+	e->data = to_push[4];
+	e->left = NULL;
+	e->right = NULL;
+	f->data = to_push[5];
+	f->left = NULL;
+	f->right = NULL;
+	d->left = e;
+	d->right = f;
+	node *g = (node*)malloc(sizeof(node));
+	node *h = (node*)malloc(sizeof(node));
+	g->data = to_push[6];
+	g->left = NULL;
+	g->right = NULL;
+	h->data = to_push[7];
+	h->left = NULL;
+	h->right = NULL;
+	c->left = g;
+	c->right = h;
+	return a;
+
+}
+
+void recur_preorder(node *root){
+	if(root != NULL){
+		std::cout << root->data;
+		recur_preorder(root->left);
+		recur_preorder(root->right);
+	}
+
+}
+
+void recur_inorder(node *root){
+	if(root != NULL){
+		recur_inorder(root->left);
+		std::cout << root->data;
+		recur_inorder(root->right);
+	}
+}
+
+void recur_postorder(node *root){
+	if(root != NULL){
+		recur_postorder(root->left);
+		recur_postorder(root->right);
+		std::cout << root->data;
+	}
+}
+
+int main(){
+	char to_push[9]= {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'};
+	node *root = NULL;
+
+	root = make_tree(to_push);
+
+	// These are all recursive traversals
+	std::cout << "preorder: ";
+	recur_preorder(root);
+	std::cout << "\ninorder: ";
+	recur_inorder(root);
+	std::cout << "\npostorder: ";
+	recur_postorder(root);
+	return 0;
+}
