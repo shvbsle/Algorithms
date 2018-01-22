5019ceeb2f8c5c1759c9732c765a10f544252094
shiv <shivbhosale97@gmail.com>
2018-01-22 23:14:43 +0530

    tic-tac-toe non AI play as X or O added

--- ttt_nonai.py
+++ ttt_nonai.py
@@ -1,6 +1,8 @@
 '''
 SHIV's
 Tic Tac Toe game using a non AI method
+
+Haha... So damn easy. My code rocks
 '''
 
 # this will be my board
@@ -55,14 +57,20 @@ def poswin(symbol):
 	# no winning move possible :( return 0
 	return 0
 
-toggle = True
+player = input("select symbol (X or O) (X plays first): ")
+opp = 'X'
+toggle = False
+if player == 'X':
+	opp = 'O'
+	toggle = True
+
 # main game loop
 to, tx = 0, 0
 for i in range(9):
 	if toggle:
 		print("player makes move")
 		toggle = False
-		while not go('X', int(input("make a move [0-8]:"))):
+		while not go(player, int(input("make a move [0-8]:"))):
 			print("invalid move. Try again!")
 		show_board()
 		tx+=1
@@ -70,33 +78,38 @@ for i in range(9):
 		print("computer moves")
 		toggle = True
 		if board[4] == '_' and tx < 2:
-			go('O', 4)
+			go(opp, 4)
 		elif tx < 2:
-			go('O', next(corner('O')))
+			go(opp, next(corner(opp)))
 
 		# don't let opponent win
-		if tx == 2:
-			if poswin('X') != 0:
-				go('O', poswin('X'))
+		if tx == 2 and to != tx:
+			if poswin(player) != 0:
+				go(opp, poswin(player))
 			else:
-				go('O', next(corner('O')))
+				go(opp, next(corner(opp)))
 
 		# now win for yourself
-		if to >= 2:
-			if poswin('O') != 0:
-				go('O', poswin('O'))
+		if to >= 2	:
+			if poswin(opp) != 0:
+
+				print("heheiehi")
+				go(opp, poswin(opp))
 				show_board()
 				print("hahah Computer wins!")
 				break
-			elif poswin('X') != 0:
-				go('O', poswin('X'))
+			elif poswin(player) != 0:
+				go(opp, poswin(player))
+				to+=1
+				show_board()
+				continue
 			else:
-				val = go('X', next(corner('X')))
+				val = go(opp, next(corner(opp)))
 				if val == -1:
-					go('X', next(edge('X')))
+					go(opp, next(edge(opp)))
 		to+=1
 		show_board()
-	print(to, ":to ",tx, ":tx")
+	print(to, ":comp ",tx, ":player")
 
 
 
