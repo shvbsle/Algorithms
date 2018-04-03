0df05a24bd3418375f8d39cbc245bdbb3302c290
shiv <shivbhosale97@gmail.com>
2018-04-03 20:47:07 +0530

    added a properly working DES code in CPP

--- des.cpp
+++ des.cpp
@@ -25,7 +25,7 @@ std::string s1[4][4] = {{"00","01","10","11"},
 
 // global keys that I have to generate
 std::pair<std::string, std::string> keys;
-
+std::pair<std::string, std::string> temp_keys;
 // key generation function
 std::pair<std::string, std::string> generate_keys(std::string key_str){
 	std::pair<std::string, std::string> answer;
@@ -35,33 +35,35 @@ std::pair<std::string, std::string> generate_keys(std::string key_str){
 	for(int i = 0 ; i < key_str.length(); i++){
 		temp[i] = key_str.at(p10[i+1]-1);
 	}
-
+	std::cout << "After P10: "<< temp << "\n";
 	std::string r = temp.substr(0,5);
 	std::string l = temp.substr(5,9);
 	// LS-1
 	r = r.substr(1,5) + r.substr(0,1);
 	l = l.substr(1,5) + l.substr(0,1);
 
-	// std::cout << r << "|" << l << "\n";
+	std::cout <<"After LS1: " << r << "|" << l << "\n";
 	std::string t2 = r+l, k1 = r+l;
 	for(int i = 0; i< 8; i++){
 		k1[i] = t2.at(p8[i+1]-1);
 	}
 	// std::cout << k1;
+	std::cout << "After P8: " << k1 << "\n"; 
 
 	// LS-2
 	r = r.substr(1,5) + r.substr(0,1);
 	l = l.substr(1,5) + l.substr(0,1);
 	r = r.substr(1,5) + r.substr(0,1);
 	l = l.substr(1,5) + l.substr(0,1);
-	// std::cout << r << "|" << l << "\n";
+	std::cout <<"After LS2: "<< r << "|" << l << "\n";
 	std::string k2 = r+l;
 	t2 = r+l;
 	for(int i = 0; i< 8; i++){
 		k2[i] = t2.at(p8[i+1]-1);
 	}
+	std::cout << "After P8: " << k2 << "\n";
 
-	// std::cout << k1.substr(0,8) << "\n" << k2.substr(0,8);
+	std::cout <<"Keys are:" << k1.substr(0,8) << ", " << k2.substr(0,8) << "\n";
 	answer.first = k1.substr(0,8);
 	answer.second = k2.substr(0,8);
 	return answer;
@@ -73,14 +75,14 @@ std::string encrypt(std::string plaintext){
 	for(int i = 0 ; i < plaintext.length(); i++){
 		temp[i] = plaintext.at(ip[i+1]-1);
 	}
-	// std::cout << temp;
+	std::cout <<"After IP:"<< temp <<"\n";
 	l = temp.substr(0,4);
 	r = temp.substr(4,8);
 
 	for(int i = 0 ; i < r.length()*2; i++){
 		temp[i] = r.at(EP[i+1]-1);
 	}
-
+	std::cout << "After EP: " << temp << "\n";
 	//xor with K1
 	for(int i =0; i < 8; i++){
 		if(temp.at(i) == keys.first.at(i)){
@@ -90,7 +92,7 @@ std::string encrypt(std::string plaintext){
 			temp[i] = '1';
 		}
 	}
-	// std::cout << temp << "\n";
+	std::cout <<"After XOR with K1: " << temp << "\n";
 
 	// S0 value
 	std::string row,col, t1, t2;
@@ -98,7 +100,7 @@ std::string encrypt(std::string plaintext){
 	row.push_back(temp[3]);
 	col.push_back(temp[1]);
 	col.push_back(temp[2]);// = temp[1]+temp[2];
-	// std::cout << row << " | " << col << "\n";
+	std::cout <<"S0 value:" << row << " | " << col << "\n";
 	t1 = s0[bin_vals[row]][bin_vals[col]];
 
 	// S1 value
@@ -107,7 +109,7 @@ std::string encrypt(std::string plaintext){
 	row.push_back(temp[7]);
 	col.push_back(temp[5]);
 	col.push_back(temp[6]);// = temp[1]+temp[2];
-	// std::cout << row << " | " << col << "\n";
+	std::cout <<"S1 value: "<< row << " | " << col << "\n";
 	t2 = s1[bin_vals[row]][bin_vals[col]];
 	// std::cout << t1 << "|" << t2 << "\n"; 
 	t1 = t1+t2;
@@ -115,29 +117,34 @@ std::string encrypt(std::string plaintext){
 	for(int i = 0 ; i < 4; i++){
 		t2[i] = t1.at(p4[i+1]-1);
 	}
-	// std::cout << t1;
-
+	std::cout <<"After P4: " <<  t2 << "\n";
+	// std::cout << l << "\n";
 	//xor with r
 	for(int i =0; i < 4; i++){
-		if(t1.at(i) == l.at(i)){
-			t1[i] = '0';
+		if(t2.at(i) == l.at(i)){
+			t2[i] = '0';
 		}
 		else{
-			t1[i] = '1';
+			t2[i] = '1';
 		}
 	}
-
-	l = r;
-	r = t1;
+	std::cout << "XOR with r:" << t2 << "\n";
+	// l = r;
+	// r = t1;
 
 	// round 2:
-	l = temp.substr(0,4);
-	r = temp.substr(4,8);
+	// l = temp.substr(0,4);
+	// r = temp.substr(4,8);
+	std::cout << "After Swap: "<< r+t2 << "\n";
+	temp = r+t2;
+	std::string random_temp = temp;
 
-	for(int i = 0 ; i < r.length()*2; i++){
-		temp[i] = r.at(EP[i+1]-1);
+	std::string r_temp = temp.substr(4,8);
+
+	for(int i = 0 ; i < 8; i++){
+		temp[i] = r_temp.at(EP[i+1]-1);
 	}
-
+	std::cout << "After EP: " << temp << "\n";
 	//xor with K1
 	for(int i =0; i < 8; i++){
 		if(temp.at(i) == keys.second.at(i)){
@@ -147,7 +154,7 @@ std::string encrypt(std::string plaintext){
 			temp[i] = '1';
 		}
 	}
-	// std::cout << temp << "\n";
+	std::cout <<"Round 2: XOR with K2: "<< temp << "\n";
 
 	// S0 value
 	row = col = t1 = t2 = "";
@@ -155,7 +162,7 @@ std::string encrypt(std::string plaintext){
 	row.push_back(temp[3]);
 	col.push_back(temp[1]);
 	col.push_back(temp[2]);// = temp[1]+temp[2];
-	// std::cout << row << " | " << col << "\n";
+	std::cout <<"S0 value: " << row << " | " << col << "\n";
 	t1 = s0[bin_vals[row]][bin_vals[col]];
 
 	// S1 value
@@ -164,32 +171,37 @@ std::string encrypt(std::string plaintext){
 	row.push_back(temp[7]);
 	col.push_back(temp[5]);
 	col.push_back(temp[6]);// = temp[1]+temp[2];
-	// std::cout << row << " | " << col << "\n";
+	std::cout <<"S1 value: " <<row << " | " << col << "\n";
 	t2 = s1[bin_vals[row]][bin_vals[col]];
 	// std::cout << t1 << "|" << t2 << "\n"; 
 	t1 = t1+t2;
+	std::cout << "this is t1:" << t1;
 	t2 = "0000";
 	for(int i = 0 ; i < 4; i++){
 		t2[i] = t1.at(p4[i+1]-1);
 	}
-	// std::cout << t1;
+	std::cout <<"\nAfter P4: " << t2 << "\n";
 
-	//xor with r
+	//xor with l
+	l = random_temp.substr(0,4);
 	for(int i =0; i < 4; i++){
-		if(t1.at(i) == l.at(i)){
-			t1[i] = '0';
+		if(t2.at(i) == l.at(i)){
+			t2[i] = '0';
 		}
 		else{
-			t1[i] = '1';
+			t2[i] = '1';
 		}
 	}
 
-	std::string temp2 = r+t1;
+	std::cout <<"After xor with l: " << t2 << "\n";
+	r = random_temp.substr(4,8);
+	std::string temp2 = t2+r;
+	// std::cout << temp2 << "\n";
 
 	for(int i = 0 ; i < 8; i++){
 		answer[i] = temp2.at(ip_i[i+1]-1);
 	}
-	// std::cout << answer;
+	std::cout << "After IP^-1: "<<answer<< "\n";
 
 	return answer;
 }
@@ -214,15 +226,26 @@ std::string encrypt(std::string plaintext){
 // }
 
 int main(){
-	std::string key_str = "1010000010";
+	std::string key_str = "0010010111", plaintext;
 	// this function will generate them keys biatch!
+	std::cout << "Original key: " << key_str << "\nKey generation wil now begin:\n";
 	keys = generate_keys(key_str);
 
-	// std::cout << "Enter plaintext (8 bits): ";
-	std::string plaintext = "10111101";
+	std::cout << "Enter plaintext (8 bits): ";
+	// plaintext = "10100101";
+	std::cin >> plaintext;
+	std::cout << "plaintext is: " << plaintext << "\n";
+	std::cout << "\n----------Encryption---------\n";
+	std::cout << "beginning encryption: \n";
 	std::string cipher = encrypt(plaintext);
 	std::cout << "encrypted text is: " << cipher << "\n";
-	std::cout << "decrypted text is: " << plaintext;
+	std::string tkey;
+	tkey = keys.first;
+	keys.first = keys.second;
+	keys.second = tkey;
+	std::cout << "\n----------Decryption---------\n";
+	tkey = encrypt(cipher);
+	std::cout << "decrypted text is: " << tkey;
 	// std::cin >> temp;
 	// // create a bitset of plaintext
 	// std::bitset<8> PT(temp);
