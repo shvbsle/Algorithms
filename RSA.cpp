d185193effa10a0d7e8634c02eede0a446a68f0c
shiv <shivbhosale97@gmail.com>
2018-04-21 17:25:15 +0530

    added updated RSA

--- RSA.cpp
+++ RSA.cpp
@@ -1,71 +1,158 @@
 /*
-SHIV's code for RSA n what not in cpp
+SHIV's RSA Algorithm
 */
-#include <bits/stdc++.h>
 
-// mod pow function 
-int ModPow(int x, int y, int p)
+#include<iostream>
+#include<math.h>
+#include<cstdlib>
+
+int check_prime(unsigned long int n);
+unsigned long int encrypt(unsigned long int, unsigned long int, unsigned long int);
+unsigned long int decrypt(unsigned long int, unsigned long int, unsigned long int);
+unsigned long int jpl(unsigned long int, unsigned long int, unsigned long int);
+
+
+
+int main()
 {
-	int res = 1;
-	x = x % p;
-	while (y > 0)
-	{
-	if ((y & 1) == 1){
-		res = (res*x) % p;
-		std::cout << res << " = " <<(res*x) <<  " mod(" << p << ")\n";
-	}
-	y = y>>1;
-	x = (x*x) % p;
-	}
 
-	return res;
+  double r;
+  int a;
+  unsigned long int p, q, n, phin, e, d, k;
+  unsigned long int c, m, mm;
+
+
+// generate 2 prime numbers < 100
+  
+  a = 0;
+
+  // while(a==0) { 
+  // r = ((double) rand() / (RAND_MAX));
+  // p = int(r*100.0);
+  // a = check_prime(p); 
+  // }
+
+  // a = 0;
+
+  // while(a==0) { 
+  // r = ((double) rand() / (RAND_MAX));
+  // q = int(r*100.0);
+  // a = check_prime(q); 
+  // }
+  std::cout << "Enter p: " ;
+  std::cin >> p;
+  std::cout << "Enter q: " ;
+  std::cin >> q;
+
+  std::cout<<"p,q = "<<p<<" "<<q<<"\n";
+
+  n = p*q;
+  phin = (p-1)*(q-1);
+
+
+// generate e such that 1<e<phin, e and phin are coprime
+
+ 
+  a = 0;
+
+  while(a==0) { 
+  r = ((double) rand() / (RAND_MAX));
+  e = int(0.1*r*double(phin));
+  a = check_prime(e); 
+  }
+
+  std::cout<<"public key, n, e: "<<n<<" "<<e<<" \n";
+
+
+// generate private key, d
+
+  k = 5;
+  while((1+k*phin)%e!=0){
+   k++;
+  }
+
+  d = (1 + k*phin)/e; 
+
+  std::cout<<" k, d "<<k<<" "<<d<<" \n";
+
+
+//  secret message to be encrypted
+   std::cout<<"\n enter secret message to be encrypted: \n";
+   std::cin>>m;
+
+
+//  encrypt message
+
+  c = encrypt(n,e,m); 
+
+  std::cout<<"encrypted message: "<<c<<" \n";   
+
+
+// decrypt message
+
+  mm = decrypt(n,d,c);
+
+  std::cout<<"decrypted message "<<mm<<" \n";
+
+
+
+  return 0;
 }
 
-// Mod inverse
-int ModInverse(int iNum1, int iNum2)
+
+
+// check for prime: return 1 if prime; 0 otherwise
+int check_prime(unsigned long int n)
 {
-	iNum1 = iNum1%iNum2;
-	for (int i=1; i < iNum2; i++)
-		if ((iNum1*i) % iNum2 == 1)
-			return i;
-	return 0;
+
+  unsigned long int i;
+
+  for(i = 2; i<= sqrt(n); i++)
+  {
+   if(n%i == 0){
+     return 0;
+   }
+  }
+
+  return 1;
 }
 
-#include <bits/stdc++.h>
 
-int main(){
-	int M, P, Q, E, N, phi, C, D;
-	std::cout << "Enter some message (int): ";
-	std::cin >> M;
-	std::cout << "\nP value: ";
-	std::cin >> P;
-	std::cout << "\nQ value: ";
-	std::cin >> Q;
-	std::cout << "\nE value: ";
-	std::cin >> E;
-
-	// N and phi
-	N = P*Q;
-	phi = (P-1)*(Q-1);
-
-	std::cout << "\nN: " << N << " and phi: " << phi << "\n";
-
-	std::cout << "beginning encryption: \n";
-	C = ModPow(M,E,N);
-	std::cout << "Encrypted text is: " << C << "\n";
-
-	std::cout << "beginning decryption: \n";
-
-	D = ModInverse(E, phi);
-
-	std::cout << "D is: " << D << "\n";
-
-	M = ModPow(C, D, N);
-
-	std::cout << "Decrypted value is: " << M << "\n";
-
-
-
-
-	return 0;
+// encrypt
+unsigned long int encrypt(unsigned long int n, unsigned long int e, unsigned long int m)
+{
+  unsigned long int c, i;
+  
+  c = jpl(m,e,n); 
+  return c;
 }
+
+
+// decrypt
+unsigned long int decrypt(unsigned long int n, unsigned long int d, unsigned long int c)
+{
+  unsigned long int m;
+  
+  m = jpl(c,d,n);
+  return m;
+}
+
+
+// (b^e)%m
+unsigned long int jpl(unsigned long int b, unsigned long int e, unsigned long int m)
+{
+
+ unsigned long int c, e1; 
+  
+ c = 1;
+ e1 = 0;
+
+ while(e1<e){
+  e1++;
+  c = (b*c)%m;
+ } 
+
+ return c; 
+}
+
+
