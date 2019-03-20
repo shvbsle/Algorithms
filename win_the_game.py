8a952589ff564df44015a0564e291928dfbec89f
shiv <shivjeet@capiot.com>
2019-03-20 14:14:26 +0530

    found a better equation. smaller

--- win_the_game.py
+++ win_the_game.py
@@ -8,32 +8,30 @@ sample input
 10 0
 
 '''
-import math
-
+from math import factorial
+import sys
+sys.setrecursionlimit(50000)
 tn = int(input())
 
 memo = {}
-def solve(player, rn, gn):
-	n = rn+gn
-	if (player, rn, gn) in memo:
-		return memo[player, rn, gn]
-	if n <= 1:
-		return 0
-	c1, c2 = 0, 0
-	if player == 1:
-		c1 = rn*(n-1)
-		c2 = solve(2, rn, gn-1)
-	elif player == 2:
-		c1 = gn*(n-1)
-		c2 = solve(1, rn-1, gn)
-	memo[player, rn,gn] = c1+c2
-	return c1+c2
 
+def solve(r, g):
+	if g ==0 and r>0 or r ==0 and g>0:
+		return 1.0
+	if g <0 or  r<0:
+		return 0.0
+	n = r+g-1
+	now = factorial(n)//(factorial(r-1))
+	now //= factorial(g)
+	return now + solve(r-1, g-2)
+ 
 for i in range(tn):
+	memo = {}
 	r, g = list(map(int, input().split()))
-	if r == 0 or g == 0:
-		print(1.0)
-		continue
-	x = solve(1, r, g)
-	print(round(x/math.factorial(r+g), 6))
-	
+	x = solve(r, g)
+	sl_sp = factorial(r+g)//factorial(r)
+	sl_sp //= factorial(g)
+	#print(x, sl_sp)
+	print(x/sl_sp)
+	#print(round(x/sl_sp, 6))
+	print("-----------------------------")	
