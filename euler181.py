9f9aca3aee85528f0b19592476d6acd44e0d8e30
ShivBhosale <shivbhosale97@gmail.com>
2019-06-18 00:34:42 +0530

    discovered a nice modification to the formula

--- euler181.py
+++ euler181.py
@@ -9,17 +9,36 @@ In how many ways can sixty black objects B and forty white objects W be thus gro
 
 This is definitely a counting problem. I just don't know how to count yet. So I'm gonna 
 start brute force first
+
+# discovered a nice modification to the formula
 '''
 
-nB, nW = 6,1
+nB, nW = 3,2
 
+memo_unq = {}
 def unq(n):
+    if n in memo_unq: return memo_unq[n]
+    if n<0:
+        return 0
     if n == 0:
         return 1
     ans = 0
     for i in range(1, int(n/2)+1):
         non_key = n-i
         ans+=unq(int(non_key/i))
-    return 1+ans
+    memo_unq[n] = 1+ans
+    return memo_unq[n]
 
-print(unq(8))
+# key formed by i and j
+def key(i,j):
+    keyed_ways = 0
+    for k in range(1, min(nB, nW)+1):
+        keyed_ways += unq(nB-i*k)*unq(nW-j*k)
+    return keyed_ways
+
+total_ways = unq(nB)*unq(nW)
+for i in range(1, nB+1):
+    for j in range(1, nW+1):
+        total_ways += key(i,j)
+
+print(total_ways)
