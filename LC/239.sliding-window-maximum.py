38c7893c5159efa6e1d42e0bf4d1028dcea9719d
shiv <shivbhosale97@gmail.com>
2024-07-03 16:30:59 -0400

    feat: LC/239

---
+++ LC/239.sliding-window-maximum.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=239 lang=python3
+#
+# [239] Sliding Window Maximum
+#
+
+# @lc code=start
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        
+# @lc code=end
+
--- LC/76.minimum-window-substring.py
+++ LC/76.minimum-window-substring.py
@@ -4,9 +4,20 @@
 # [76] Minimum Window Substring
 #
 
+from collections import Counter
 # @lc code=start
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        
-# @lc code=end
+        l, need, have, pos = 0, Counter(t), Counter(), [inf, '']
+        for r, e in enumerate(s):
+            have[e] += 1
+            while have >= need:
+                pos = min(pos, [r - l + 1, s[l : r + 1]])
+                have[s[l]] -= 1 
+                l+=1
 
+        return pos[1]
+     
+ # @lc code=end
+
+ 
